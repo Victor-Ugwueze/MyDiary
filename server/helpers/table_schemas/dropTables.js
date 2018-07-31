6daf3a6082ab93a2ed4f009be9df3a423dcca2b4
Victor-Ugwueze <victorugwueze@gmail.com>
2018-08-01 00:57:04 +0100

    Update user profile page
    Test endpoints for
     * Update user details
     * get total number of entries user have created
    Add  profileController and profile model
    
    Endpoint for update, /api/v1/users/profile, this is verified for token before giving acces
    Endpoint for get tottal user entries, this also verified as both are on the same router
    
    The change password, is not included
    
    feature-#159445397

--- server/helpers/table_schemas/dropTables.js
+++ server/helpers/table_schemas/dropTables.js
@@ -30,6 +30,7 @@ class SetupTestDb {
       first_name varchar(255) NOT NULL,
       last_name varchar(255) NOT NULL,
       email varchar(255) NOT NULL,
+      location varchar(255),
       password varchar(255) NOT NULL,
       created_at timestamp DEFAULT NOW()
     )`;
