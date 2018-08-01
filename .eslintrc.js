035abf2c29faca4a0c43a02ef22033870521074b
Victor-Ugwueze <victorugwueze@gmail.com>
2018-08-01 12:58:39 +0100

    Implement login authentication with front-end
    Set up, API client for making request to server
    Get input from login form
    Validate input field, show error if any
    Save token to localStorage
    Redirect to dashboard page
    
    The validation fuction will be used for signup
    
    feature-#159457618

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  
+  rules:{
+    "linebreak-style": 0
+  },
+  "parser": "babel-eslint",
+  env: {
+    "browser": true,
+    "node": true,
+    "jasmine": true
+  },
+};
