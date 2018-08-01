40467aca195ff62bccd261564b90bf3ffe31c849
Victor-Ugwueze <victorugwueze@gmail.com>
2018-08-01 20:51:15 +0100

    Setup template for documentation
    Documentation is built using swagger and npm package,
    swagger-ui-express.
    This will document all the api endpoints and example responses.
    
    chore-#159467186

---
+++ server/controllers/docsController.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/swagger';
+
+const router = express.Router();
+
+router.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+export default router;
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,12 @@
 import entryController from '../controllers/entryController';
 import userController from '../controllers/userController';
+import docsController from '../controllers/docsController';
 
 
 const mountRouter = (app) => {
   app.use('/api/v1', entryController);
   app.use('/auth', userController);
+  app.use('/', docsController);
 };
 
 export default mountRouter;
