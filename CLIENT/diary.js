3846fdab23ac0466d988e74541b4f5c12271d211
Victor-Ugwueze <victorugwueze@gmail.com>
2018-08-02 03:04:48 +0100

    Update diary entry
    Add show edit modal
    Fetch entry from database and populate, edit modal
    Click update to make request and save on database
    
    Feature-#159482520

--- CLIENT/diary.js
+++ CLIENT/diary.js
@@ -1,8 +1,39 @@
+/* global SelectElement */
+
+
+// Populate Edit button
+const populateModalFoEdit = (targetEditButton, EditModal) => {
+  DiaryClient.getSingleEntry(targetEditButton.dataset.id.split('-')[1])
+    .then((entry) => {
+      // get refrence to modal and ppulate conten
+      const editModalTitle = EditModal.querySelector('#diary-title');
+      const editModalBody = EditModal.querySelector('#diary-body');
+      const entryInputId = EditModal.querySelector('#entry-id');
+      editModalTitle.value = entry.result.title;
+      editModalBody.textContent = entry.result.body;
+      entryInputId.value = entry.result.id;
+    });
+};
+
+// Edit Diary Add Event Method
+const addEventListenerToEditButton = () => {
+  const editButtons = document.querySelectorAll('.action-edit');
+  [...editButtons].forEach((editButton) => {
+    editButton.addEventListener('click', (event) => {
+      const editDiaryModal = document.querySelector(`#${event.currentTarget.dataset.target}`);
+      SelectElement(editDiaryModal, null, 'show');
+      populateModalFoEdit(event.currentTarget, editDiaryModal);
+    });
+  });
+};
+
 
 class DiaryClient {
   static init() {
     document.querySelector('#add-entry-form')
       .addEventListener('submit', DiaryClient.addEntry);
+    document.querySelector('#edit-diary-entry-form')
+      .addEventListener('submit', DiaryClient.updateEntry);
     DiaryClient.getAllEntries();
   }
 
@@ -18,6 +49,7 @@ class DiaryClient {
         console.log(response);
         //  updatePaginate(response);
         displayListEntries(response);
+        addEventListenerToEditButton();
       })
       .catch((err) => {
         console.log(err);
@@ -50,6 +82,49 @@ class DiaryClient {
       .catch(err => err);
   }
 
+  static getSingleEntry(id) {
+    const token = DiaryClient.checkToken();
+    const method = 'get';
+    const url = `https://my-diary-dev.herokuapp.com/api/v1/entries/${id}`;
+    const data = {
+      token,
+    };
+    return makeNetworkRequest({ url, method, data })
+      .then((response) => {
+        if (response.message === 'success') {
+          return response;
+        }
+      })
+      .catch(err => err);
+  }
+
+  static updateEntry(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const id = form.get('entry-id');
+    const title = form.get('title');
+    const body = form.get('body');
+    if (title === '') {
+      return;
+    }
+    const token = DiaryClient.checkToken();
+    const method = 'put';
+    const url = `https://my-diary-dev.herokuapp.com/api/v1/entries/${id}`;
+    const data = {
+      token,
+      title,
+      body,
+    };
+    makeNetworkRequest({ url, method, data })
+      .then((response) => {
+        if (response.message === 'success') {
+          console.log(response);
+          // window.location.reload();
+        }
+      })
+      .catch(err => err);
+  }
+
   static checkToken() {
     const token = localStorage.getItem('token');
     if (!token) {
@@ -70,7 +145,7 @@ const makeNetworkRequest = (input = { url: '', method: '', data: '' }) => {
     reqObject.headers = {
       'content-type': 'application/json',
       'x-access-token': input.data.token,
-      page: 2,
+      page: 1,
       perPage: 5,
     };
   } else {
@@ -94,7 +169,7 @@ const bindEntryData = (entry) => {
   if (entry.body.length > 40) {
     EntryBody = `${entry.body.substring(0, 40)} ...`;
   }
-  const entryWrap = `<li class="entry-item" id="diary-1">
+  const entryWrap = `<li class="entry-item" id="diary-${entry.id}">
                       <div class="" data-id="diary-${entry.id}" data-target="view-single-diary">
                           <h4 class="sing-diary-title diary-text">${EntryTitle}</h4>
                           <p class="sing-diary-body diary-text">${EntryBody}</p>
@@ -102,11 +177,15 @@ const bindEntryData = (entry) => {
                       <p class="created-at">12/18/2018</p>
                       <a class="action">
                         <span data-id="diary-${entry.id}" data-target="edit-diary-entry" class="btn btn-primary action-edit">
-                          <span> <img class="diary-edit icon-edit" src="Resources/images/edit.png">
-                          </span><span class="edit-text">Edit</span></span> | <span data-target="diary-${entry.id}" class="btn btn-danger action-delete">
-                            <span> <img class="diary-edit icon-edit" src="Resources/images/delete-button.png">
-                            </span><span class="delete-text">Delete</span>
-                          </a>
+                          <img class="diary-edit icon-edit" src="Resources/images/edit.png">
+                          <span class="edit-text">Edit</span>
+                        </span>
+                         | 
+                        <span data-target="diary-${entry.id}" class="btn btn-danger action-delete">
+                          <img class="diary-edit icon-edit" src="Resources/images/delete-button.png">
+                          <span class="delete-text">Delete</span>
+                        </span>
+                      </a>
                       <div class="arrow-up"></div>
                   </li>`;
   return entryWrap;
@@ -118,6 +197,7 @@ const displayListEntries = (response) => {
     listContainer.innerHTML += (bindEntryData(element));
   });
 };
+// Add EventListener to element after loading
 
 
 DiaryClient.init();
--- UI/Resources/js/dashboard.js
+++ UI/Resources/js/dashboard.js
@@ -1,145 +1,113 @@
+/* global SelectElement, modal */
 
-
-//Hide or show Menu bar
+// Hide or show Menu bar
 const iconbars = document.querySelectorAll('.menu-icon');
-const navigationMenu =  document.querySelector('#left-section .nav-tabs');
+const navigationMenu = document.querySelector('#left-section .nav-tabs');
 const rightSection = document.querySelector('#right-section');
 
 [...iconbars].forEach((iconbar) => {
-    iconbar.addEventListener('click', () => {
-        navigationMenu.classList.toggle('nav-show')
-        rightSection.classList.toggle('full-width');
-     });
+  iconbar.addEventListener('click', () => {
+    navigationMenu.classList.toggle('nav-show');
+    rightSection.classList.toggle('full-width');
+  });
 });
 
 const navigationMenuButtons = document.querySelectorAll('.nav-it');
 // console.log(document.querySelector('.nav-it').hasAttribute('data-target'));
 
 [...navigationMenuButtons].forEach((element) => {
-    element.addEventListener('click', (event) => {
-      let navMenuitem = event.target;
-      if(!navMenuitem.classList.contains('nav-it')){
-        navMenuitem = navMenuitem.parentNode;
-      }
-        const  allItems = document.querySelectorAll('.nav-it');
-        // Change Active Navigation tab
-        SelectElement(navMenuitem, [...allItems],'active');
-        // Select tab content
-        const targetPageSection = document.querySelector(`#main-section #${navMenuitem.dataset.target}`);
-        const allPageSections = document.querySelectorAll('.tab-pane');
-        SelectElement(targetPageSection, [...allPageSections], 'selected');
-    })
-})
+  element.addEventListener('click', (event) => {
+    let navMenuitem = event.target;
+    if (!navMenuitem.classList.contains('nav-it')) {
+      navMenuitem = navMenuitem.parentNode;
+    }
+    const allItems = document.querySelectorAll('.nav-it');
+    // Change Active Navigation tab
+    SelectElement(navMenuitem, [...allItems], 'active');
+    // Select tab content
+    const targetPageSection = document.querySelector(`#main-section #${navMenuitem.dataset.target}`);
+    const allPageSections = document.querySelectorAll('.tab-pane');
+    SelectElement(targetPageSection, [...allPageSections], 'selected');
+  });
+});
 
-//Show add entry modal
+// Show add entry modal
 const addNewEntryButtons = document.querySelectorAll('.add-entry');
 const newEntrybuttons = [...addNewEntryButtons];
 
 newEntrybuttons.forEach((button) => {
-  button.addEventListener('click', (event) =>{
+  button.addEventListener('click', (event) => {
     const modalToshow = document.querySelector(`#${event.target.dataset.target}`);
-    SelectElement(modalToshow,null,'show');
+    SelectElement(modalToshow, null, 'show');
   });
 });
-
 
 
 const closeModal = (event) => {
   const modalToshow = document.querySelector(`#${event.target.dataset.target}`);
   modal.hide(modalToshow, 'show');
-}
-//close modal function
+};
+// close modal function
 const modalCloseButton = document.querySelectorAll('.modal .close');
 [...modalCloseButton].forEach((closeButton) => {
-    closeButton.addEventListener('click', closeModal)
+  closeButton.addEventListener('click', closeModal);
 });
 
-//Edit dairy entry 
 
-const editButtons = document.querySelectorAll('.action-edit');
-
-
-[...editButtons].forEach((editButton)=> {
-    editButton.addEventListener('click', (event) => {
-        const EditDiaryModal = document.querySelector(`#${event.target.dataset.target}`);
-        SelectElement(EditDiaryModal,null,'show');
-        populateModalFoEdit(this,EditDiaryModal)
-    })
-})
-
-const populateModalFoEdit = (targetEditButton,EditModal)=> {
-    //get the diary content
-    const dairyItem = document.querySelector(`#${targetEditButton.dataset.id}`);
-    const dairyTitle = dairyItem.querySelector('.sing-diary-title');
-    const diaryBody = dairyItem.querySelector('.sing-diary-body');
-
-    //get refrence to modal and ppulate content
-    
-    const EditModalTitle = EditModal.querySelector('#diary-title');
-    const EditModalBody = EditModal.querySelector('#diary-body');
-
-    EditModalTitle.value = dairyTitle.innerHTML;
-    EditModalBody.textContent = diaryBody.innerHTML;
-}
-
-
-//Delete a modal Entry
+// Delete a modal Entry
 const deleteEntryButtons = document.querySelectorAll('.action-delete');
 
-[...deleteEntryButtons].forEach((deleteEntryButton)=> {
-    deleteEntryButton.addEventListener('click', (event) => {
-        deleteModalItem(event.target)
-    })
-})
-const deleteModalItem = function (targetDeleteButton){
-    
-    const diaryList = document.querySelector('#dairy-entries');
-    const confirmDeleteBox = document.querySelector('#confirm-delete');
-    confirmDelete = confirmDeleteBox.querySelector('.dailog-ok');
-    modal.show(confirmDeleteBox,'show');
-    confirmDeleteBox.addEventListener('click',()=>{
-        const diaryItem = diaryList.querySelector(`#${targetDeleteButton.dataset.target}`);
-        modal.hide(confirmDeleteBox,'show');
-    })
-   
-}
+[...deleteEntryButtons].forEach((deleteEntryButton) => {
+  deleteEntryButton.addEventListener('click', (event) => {
+    deleteModalItem(event.target);
+  });
+});
 
-//show a single Diary Entry
+const deleteModalItem = (targetDeleteButton) => {
+  const diaryList = document.querySelector('#dairy-entries');
+  const confirmDeleteBox = document.querySelector('#confirm-delete');
+  // confirmDelete = confirmDeleteBox.querySelector('.dailog-ok');
+  modal.show(confirmDeleteBox, 'show');
+  confirmDeleteBox.addEventListener('click', () => {
+    const diaryItem = diaryList.querySelector(`#${targetDeleteButton.dataset.target}`);
+    modal.hide(confirmDeleteBox, 'show');
+  });
+};
+
+// show a single Diary Entry
 
 const showEntryButtons = document.querySelectorAll('.diary-text');
-[...showEntryButtons].forEach((showEntryButton)=>{
-    showEntryButton.addEventListener('click', (event) => {
-        showDiaryEntry(event.target.parentNode)
-    })
-})
+[...showEntryButtons].forEach((showEntryButton) => {
+  showEntryButton.addEventListener('click', (event) => {
+    showDiaryEntry(event.target.parentNode);
+  });
+});
 
 const showDiaryEntry = (containerDiv) => {
-    //Make network request with entry id
-    const itemId = containerDiv.dataset.id;
-    const viewEntryModal = document.querySelector(`#${containerDiv.dataset.target}`);
-    //get refrence to elements 
-    const titleContainer = viewEntryModal.querySelector('#diary-content h4');
-    const bodyContainer = viewEntryModal.querySelector('#diary-content p');
-    const DateContainer = viewEntryModal.querySelector('#date');
-    
-    modal.show(viewEntryModal,'show');
-}
+  // Make network request with entry id
+  const itemId = containerDiv.dataset.id;
+  const viewEntryModal = document.querySelector(`#${containerDiv.dataset.target}`);
+  // get refrence to elements
+  const titleContainer = viewEntryModal.querySelector('#diary-content h4');
+  const bodyContainer = viewEntryModal.querySelector('#diary-content p');
+  const DateContainer = viewEntryModal.querySelector('#date');
+  modal.show(viewEntryModal, 'show');
+};
 
-//Show logout box
+// Show logout box
 
 const dropDownImage = document.querySelector('.nav-link img');
-dropDownImage.addEventListener('click',()=>{
-    window.addEventListener('click',(event)=>{
-        //Click away to close logout box
-        if(event.clientX<648 || event.clientY>81){
+dropDownImage.addEventListener('click', () => {
+  window.addEventListener('click', (event) => {
+    // Click away to close logout box
+    if (event.clientX < 648 || event.clientY > 81) {
+      dropDownImage.nextElementSibling.firstElementChild.classList.remove('show');
+    }
+  });
+  modal.show(dropDownImage.nextElementSibling.firstElementChild, 'show');
+});
 
-            dropDownImage.nextElementSibling.firstElementChild.classList.remove('show');
-        }
-    })
-   modal.show(dropDownImage.nextElementSibling.firstElementChild,'show');
-})
-
-//update profile details
+// update profile details
 
 const profileInputItems = document.querySelectorAll('.profile-details input');
 const inputs = [...profileInputItems];
@@ -150,10 +118,8 @@ inputs.forEach((input) => {
     saveButton.removeAttribute('disabled');
   });
 });
- 
-//make network request to update profile
+
+// make network request to update profile
 saveButton.addEventListener('click', () => {
 //
 });
-
-
