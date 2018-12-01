6fdf0ce6422e1604fb53746bc42c3566619aa3c2
victor <victorugwueze@gmail.com>
2018-12-01 05:28:13 +0100

    chore(user auth): return user object after after authentication
    [Finishes #162361682]

--- server/models/entry.js
+++ server/models/entry.js
@@ -3,10 +3,10 @@ import { formatEntryDate, trimeSpaces } from '../helpers/entryHelper';
 
 class Entry {
   /**
- * Represents an entry.
- * @constructor
- *
- */
+   * Represents an entry.
+   * @constructor
+   *
+   */
 
   constructor() {
     this.title = null;
@@ -26,7 +26,8 @@ class Entry {
       text: 'SELECT * FROM entries WHERE user_id = $1 AND id = $2',
       values: [this.userId, requestId],
     };
-    return this.pool.query(query)
+    return this.pool
+      .query(query)
       .then((result) => {
         if (result.rows[0]) {
           return formatEntryDate(result.rows[0]);
@@ -37,18 +38,22 @@ class Entry {
   }
 
   /**
-     * This method finds updates an entry.
-     * @method
-     * @param {object} request - The request object.
-     *
-     */
+   * This method finds updates an entry.
+   * @method
+   * @param {object} request - The request object.
+   *
+   */
   update(request) {
     const query = {
       text: `UPDATE entries SET title = $1, 
       body = $2 WHERE user_id = $3 and id = $4 
       `,
-      values: [trimeSpaces(request.body.title),
-        trimeSpaces(request.body.body), this.userId, request.params.id],
+      values: [
+        trimeSpaces(request.body.title),
+        trimeSpaces(request.body.body),
+        this.userId,
+        request.params.id,
+      ],
     };
     let date = null;
     this.find(request.params.id)
@@ -56,22 +61,23 @@ class Entry {
         date = result.created_at;
         const date1 = new Date(date);
         const date2 = new Date();
-        if (date1.getFullYear === date2.getFullYear
-          && date1.getMonth === date2.getMonth
-          && date1.getDay === !date2.getDay
+        if (
+          date1.getFullYear === date2.getFullYear
+        && date1.getMonth === date2.getMonth
+        && date1.getDay === !date2.getDay
         ) {
           return false;
         }
       });
-    return this.pool.query(query)
+    return this.pool
+      .query(query)
       .then((result) => {
         if (result.rowCount === 1) {
           return this.find(request.params.id);
         }
         return false;
       })
-      .catch(() => {
-      });
+      .catch(() => {});
   }
 
   /**
@@ -90,11 +96,12 @@ class Entry {
       query.text = 'SELECT * FROM entries where user_id = $1 ORDER BY id DESC LIMIT $2';
       query.values = [this.userId, entryPerPage];
     } else {
-      const start = ((currentPage * entryPerPage) - entryPerPage);
+      const start = currentPage * entryPerPage - entryPerPage;
       query.text = 'SELECT * FROM entries where user_id = $1 ORDER BY id DESC LIMIT $2 OFFSET $3';
       query.values = [this.userId, entryPerPage, start];
     }
-    return this.pool.query(query)
+    return this.pool
+      .query(query)
       .then((result) => {
         const entries = result.rows.map(entry => formatEntryDate(entry));
         return entries;
@@ -103,18 +110,20 @@ class Entry {
   }
 
   /**
- * This method saves a new entry.
- * @method
- * @param {object} input - It conatains details of new entry to be saved.
- *
- */
+   * This method saves a new entry.
+   * @method
+   * @param {object} input - It conatains details of new entry to be saved.
+   *
+   */
   save(input) {
     const query = {
-      text: 'INSERT INTO entries (title,body,user_id) VALUES($1, $2, $3) RETURNING id,title,body,created_at',
+      text:
+        'INSERT INTO entries (title,body,user_id) VALUES($1, $2, $3) RETURNING id,title,body,created_at',
       values: [trimeSpaces(input.title), trimeSpaces(input.body), input.userId],
     };
 
-    return this.pool.query(query)
+    return this.pool
+      .query(query)
       .then(result => result)
       .catch(err => err);
   }
@@ -130,11 +139,11 @@ class Entry {
       text: 'DELETE FROM entries WHERE id = $1 AND user_id = $2',
       values: [requestId, this.userId],
     };
-    return this.pool.query(query)
+    return this.pool
+      .query(query)
       .then(result => result)
       .catch(err => err);
   }
 }
-
 
 export default Entry;
